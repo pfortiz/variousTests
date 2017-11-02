b0a50360ab3b18a4f3fd77c4fec3f9c446f6570e
Patricio Ortiz <patriciofortiz@gmail.com>
2017-11-02 17:28:06 +0000

    custom plots? tick

--- learnD3/uflo001.pl
+++ learnD3/uflo001.pl
@@ -335,9 +335,14 @@ var siteID = [$siteIDlist];
 var siteLat = [$siteLatlist];
 var siteLon = [$siteLonlist];
 var zindex = 1000;
+var mindex = 0;
 var fullBoxX = 50;
 var fullBoxY = 50;
 var siteGeoLoc = {};
+
+// variable to keep variables to plot together as a function of time
+var wishList = {};
+
 var divH = $divH;
 $jsGeolocation
 
@@ -426,14 +431,17 @@ for( var s = 0; s < siteID.length; s++){
     cont = siteContent[site];
     for (var c = 0; c < cont.length; c++){
         qtty = cont[c];
-        tag = site + "_" + qtty;
+        tag = site + "_" + cont[c];
+//        tag = site + "_" + qtty;
 //        console.info("Defining: " + tag );
-        unit = siteUnits[site][c];
-        uzi = siteUCD[site][c];
+//        unit = siteUnits[site][c];
+//        uzi = siteUCD[site][c];
 //        console.info("Defining: " + tag + " " + unit + " " + uzi);
         var meta = {};
-        meta.unit = unit;
-        meta.ucd = uzi;
+//        meta.unit = unit;
+//        meta.ucd = uzi;
+        meta.ucd = siteUCD[site][c];
+        meta.unit = siteUnits[site][c];
         mdata[tag] = meta;
     }
 }
@@ -515,7 +523,7 @@ function createInfoWindowContent(eventu, siteID, slat, slon){
         len = data[k].length -1;
 
         content += "<tr><td><input type='checkbox' onClick='makePlot1(this, event)' name='" +kPrime+ "' value=' '><span>" + q + "</span></td><td>" + data[k][len] + " [" + u + "]</td>";
-        content += "<td><input type='checkbox'></td></tr>";
+        content += "<td><input type='checkbox' onClick='markMe(this, event)' name='" +k + "' value=' '></td></tr>";
     }
 //onMouseOver="changeLabel('the Update')">
     content += "</table></small>";
@@ -632,6 +640,12 @@ Data for $tmday/$tsmon/$tyear  zoom = $mapZoom $slon $slat
 <div id="map"></div>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8Lq5FuZylrN-pO73Fv5_UU-Lg83N_vMY&callback=initMap">
 </script>
+<div id="wishes" style="visibility: hidden; float: right">
+<input name="plotWish" type="button" value="Plot selected"
+onClick="plotSelected()">
+<input name="clearWish" type="button" value="Clear selected"
+onClick="clearSelected()">
+</div>
 <div id="mdiv"></div>
 $canvases
 
@@ -782,6 +796,381 @@ function changeLabel(message){
 
 }
 
+function textSize(text) {
+    if (!d3) return;
+    var container = d3.select('body').append('svg');
+    container.append('text')
+                .attr("x", -99999)
+                .attr("y", -99999)
+                .attr("font-size", "12px")
+                .text(text);
+    var size = container.node().getBBox();
+    container.remove();
+    return { width: size.width, height: size.height };
+}
+
+
+function plotSelected(){
+    things = Object.keys(wishList);
+    console.info("wishList to plot: "+ things );
+    var qToPlot = {};
+    var siteColour = {};
+    for(var i = 0; i < things.length; i++){
+        parts = things[i].split("_");
+        console.info("Dealing with: " + things[i] + " " + parts);
+//        qToPlot[parts[i]] += things[i] + " ";
+        if( qToPlot[parts[1]] == null){
+            qToPlot[parts[1]] = parts[0];
+        } else {
+            text = " " + parts[0];
+            qToPlot[parts[1]] += text;
+        }
+        siteColour[parts[0]] = 1;
+    }
+    content = Object.keys(qToPlot);
+    sites = Object.keys(siteColour);
+    for(var i = 0; i < content.length; i++){
+        cle = content[i];
+        console.info("For " + cle + " I need: " + qToPlot[cle]);
+    }
+    nPlots = content.length;
+    console.info("Number of plots needed: " + nPlots);
+    var minTime = [];
+    var maxTime = [];
+    var siteTime = {};
+    nAllSites = sites.length;
+    frac = 1./(nAllSites+0);
+    for(var i = 0; i < sites.length; i++){
+        site = sites[i];
+        console.info("Site: "+  site);
+        siteColour[site] = d3.interpolateRainbow( i* frac);
+        console.info("Colour: " + site + " " + siteColour[site] + " " +(i*frac));
+        tName = site + "_Time";
+        tbName = site + "_Time_base";
+        timeOffset = Number(data[tbName]);
+
+        var timex = new Array(data[tName].length);
+        for(var t = 0; t < timex.length; t++){
+           timex[t] = (data[tName][t] + timeOffset) * 1000.;
+        }
+        siteTime[site] = timex;
+        minTime.push( d3.min(timex) );
+        maxTime.push( d3.max(timex) );
+    }
+    mint = d3.min(minTime);
+    maxt = d3.max(maxTime);
+    cName = "canvas_Multi" + mindex;
+    mindex++;
+    createDivIfNeeded(cName);
+    console.info("Pallete: " + siteColour);
+    console.info("Time range: " + mint + " to " + maxt);
+    xpadding = (maxt - mint)/20.;
+    minDate = new Date(1.0* (mint-xpadding));
+    maxDate = new Date(1.0* (maxt+xpadding));
+    var mdao = new Date((maxt+mint)/2.0);
+    midDate = mdao.getDate()  + " / " + (mdao.getMonth()+1) +  " / " + mdao.getFullYear();
+    console.info("Date range: " + minDate + ", " + maxDate);
+    console.info("Mean date: " + mdao + " " + midDate);
+
+
+    xLoc = fullBoxX;
+    yLoc = fullBoxY;
+    cWidth = 400;
+    cHeight = 80 + nPlots * 120 + nAllSites*10;
+    canvas = document.getElementById(cName);
+    canvas.style.left = xLoc + "px";
+    canvas.style.top  = yLoc + "px";
+    canvas.style.position = "absolute";
+    canvas.style.visibility = "visible";
+    canvas.style.border= "2px solid green";
+    canvas.style.height = cHeight + "px";
+    canvas.style.width = cWidth + "px";
+    canvas.style.zIndex = zindex;
+    canvas.style.backgroundColor = "#eeffee";
+    geo1 = plotGeometry(nPlots, cWidth, cHeight, 0);
+    zindex++;
+
+
+    console.info("things: "+ nPlots + " " + content);
+
+// console.info("Time offset: ", timeOffset);
+//
+// content = siteContent[leName];
+//
+//
+// console.info("X limits-1: " + timex[0] + " " + timex[timex.length-1] );
+// console.info("X limits: " + mint + " " + maxt + " " + xpadding);
+
+    canvas.innerHTML = "";
+    var svg = d3.select("#"+cName)
+                .append("svg")
+                .attr("width", cWidth)
+                .attr("height", cHeight);
+
+    var xscale = d3.scaleTime()
+                    .domain([minDate, maxDate])
+                    .range([geo1[0].xLeft, geo1[0].xRight]);
+
+    var xmargin = 0.;
+    nPm1 = nPlots -1;
+    svg.append("text")
+       .attr("transform", "translate(0,0)")
+       .attr("x", cWidth/2)
+       .attr("y", 0).attr("dy","1.5em")
+       .attr("font-size", "14px")
+       .attr("text-anchor", "middle")
+       .text("Customised Charts for " + midDate);
+
+    tlabel = sites[0];
+    var nSitesPerLine = [];
+    maxWidth = geo1[0].width;
+    accountedSites = 0;
+    for(var x = 1; x < nAllSites; x++){
+        site = sites[x];
+        try1 = tlabel + " " + site;
+        console.info("tlabel:" + tlabel + " try: " + try1);
+        tw = textSize(try1);
+        console.info("width(" + try1 + ") = " + tw.width +"/" + maxWidth);
+        if(tw.width > maxWidth){
+            console.info("Oops, it doesn't fit. do use " + tlabel);
+            nsitios =  tlabel.split(" ").length ;
+            nSitesPerLine.push( nsitios );
+            accountedSites += nsitios;
+            tlabel = site;
+        } else {
+            console.info("AOK for " + try1);
+            tlabel = try1;
+        }
+    }
+    if(accountedSites < nAllSites){
+        nSitesPerLine.push( nAllSites - accountedSites ) ;
+    }
+    sid = 0;
+    dyval = 3.0;
+    nspl = nSitesPerLine.length;
+    geo = plotGeometry(nPlots, cWidth, cHeight, nspl*15);
+    for(var l =0; l < nspl; l++){
+        npl = nSitesPerLine[l];
+        console.info("putting " + npl + " sites in line " + l);
+        xStep = geo[0].width/npl;
+        dy = dyval + "em";
+        dyval += 1.2;
+        for(var spl = 0; spl < npl; spl++){
+            xLoc = geo[0].xLeft + spl * xStep;
+            site = sites[sid];
+            color = siteColour[site];
+            svg.append("text")
+               .style("fill", color)
+               .attr("transform", "translate(0,0)")
+               .attr("x", xLoc)
+               .attr("y", 0).attr("dy", dy)
+               .attr("font-size", "12px")
+               .attr("text-anchor", "start")
+               .text(site);
+            sid++;
+        }
+    }
+//    dyval = -1.0;
+//    for(var x = 0; x < nAllSites; x++){
+//        site = sites[x];
+//        dy = dyval + "em";
+//        color = siteColour[site];
+//        svg.append("text")
+//           .style("fill", color)
+//           .attr("transform", "translate(0,0)")
+//           .attr("x", cWidth/2)
+//           .attr("y", cHeight).attr("dy", dy)
+////           .attr("y", cHeight).attr("dy","-0.7em")
+//           .attr("font-size", "12px")
+//           .attr("text-anchor", "middle")
+//           .text(site);
+//       dyval -= 1.2;
+//    }
+
+    for (var c = 0; c < nPlots ; c++){
+        gu = geo[c];
+        cont = content[c];
+        parts = qToPlot[cont].split(" ");
+        nSites = parts.length;
+        console.info("For " + cont + " I need: " + nSites + " " + parts);
+        minVal = [];
+        maxVal = [];
+        for (var z = 0; z < nSites ; z++){
+            dName = parts[z] + "_" + cont;
+            console.info("addressing: " + dName);
+            minVal.push( d3.min(data[dName]) );
+            maxVal.push( d3.max(data[dName]) );
+        }
+        miny = d3.min(minVal);
+        maxy = d3.max(maxVal);
+        console.info("Extreme for : " + cont + " " + miny + " , " + maxy);
+
+        // In conditions to draw the frame after defining the scales
+        ypadding = (maxy - miny)/20.;
+        console.info("Y limits: " + miny + " " + maxy + " " + ypadding);
+        var yscale = d3.scaleLinear()
+                .domain([miny-ypadding, maxy+ypadding])
+                .range([gu.yBot, gu.yTop]);
+        var x_axis;
+        var x_axisT;
+        if( c == 0 ){
+            x_axis = d3.axisBottom().scale(xscale).tickSize(-5,0).tickFormat("");
+            x_axisT = d3.axisTop().scale(xscale).tickSize(-5,0);
+        } else if (c == nPm1){
+            x_axis = d3.axisBottom().scale(xscale);
+            x_axisT = d3.axisTop().scale(xscale).tickSize(-5,0).tickFormat("");
+        } else {
+            x_axis = d3.axisBottom().scale(xscale).tickSize(-5,0).tickFormat("");
+            x_axisT = d3.axisTop().scale(xscale).tickSize(-5,0).tickFormat("");
+        }
+
+        var y_axis = d3.axisLeft()
+            .scale(yscale)
+            .ticks(5)
+            .tickSize(-5,0);
+        var y_axisR = d3.axisRight()
+            .ticks(5).scale(yscale).tickSize(-5,0);
+            //.tickFormat("");
+    
+        svg.append("g")
+           .attr("transform", "translate(" + gu.xLeft + ", "+ 0.0 + ")")
+           .call(y_axis);
+    
+        svg.append("g")
+           .attr("transform", "translate(" + gu.xRight + ", "+ 0.0 + ")")
+           .call(y_axisR);
+    
+        svg.append("g")
+                .attr("transform", "translate("+ xmargin +", " + gu.yBot  +")")
+                .call(x_axis)
+    
+        svg.append("g")
+           .attr("transform", "translate("+xmargin+", " + gu.yTop + ")")
+           .call(x_axisT);
+    
+        console.info(" trace after painting axes for : " + cont );
+        svg.append("text")
+           .attr("transform", "translate("+geo[0].xLeft/3+","+ gu.midY +")rotate(-90)")
+           .attr("font-size", "12px")
+           .attr("text-anchor", "middle")
+           .text(cont );
+//           .text(cont + " [" + unit + "]");
+        console.info("double check nSites: " + nSites );
+        for (var z = 0; z < nSites ; z++){
+            site = parts[z];
+            color = siteColour[site];
+            dName = site + "_" + cont;
+            console.info("Plotting: " + dName + " colour: " + color);
+            yData = data[dName];
+            timex = siteTime[site];
+            nPoints = timex.length;
+            for(var j=0; j < nPoints; j++){
+                svg.append("g").append("circle")
+                .attr("cx", xscale(timex[j]) )
+                .attr("cy", yscale(yData[j]) )
+                .attr("r", 2)
+                .attr("fill", color);
+//                .attr("fill", laScale(yData[j]));
+    //          .attr("fill", "red");
+            }
+        }
+    }
+ return;
+// var laScale;
+ nPoints = timex.length;
+// units = siteUnits[leName];
+// ucds = siteUCD[leName];
+// for (var i = 0; i < nPlots ; i++){
+//    cont = content[i];
+//    dName = leName + "_" + cont;
+//    unit = units[i];
+//    laScale = cScales[ucds[i]];
+//    gu = geo[i];
+//    console.info("Plot " +dName + " " + unit + " " + i + " " +  gu.yTop +  " " +  gu.yBot);
+//    yData = data[dName];
+//    miny = d3.min(yData);
+//    maxy = d3.max(yData);
+//    ypadding = (maxy - miny)/20.;
+//    console.info("Y limits: " + miny + " " + maxy + " " + ypadding);
+//    var yscale = d3.scaleLinear()
+//                   .domain([miny-ypadding, maxy+ypadding])
+//                   .range([gu.yBot, gu.yTop]);
+//    var x_axis;
+//    var x_axisT;
+//    if( i == 0 ){
+//        x_axis = d3.axisBottom().scale(xscale).tickSize(-5,0).tickFormat("");
+//        x_axisT = d3.axisTop().scale(xscale).tickSize(-5,0);
+//    } else if (i == nPm1){
+//        x_axis = d3.axisBottom().scale(xscale);
+//        x_axisT = d3.axisTop().scale(xscale).tickSize(-5,0).tickFormat("");
+//    } else {
+//        x_axis = d3.axisBottom().scale(xscale).tickSize(-5,0).tickFormat("");
+//        x_axisT = d3.axisTop().scale(xscale).tickSize(-5,0).tickFormat("");
+//    }
+//
+//    var y_axis = d3.axisLeft()
+//        .scale(yscale)
+//        .ticks(5)
+//        .tickSize(-5,0);
+//    var y_axisR = d3.axisRight()
+//        .ticks(5).scale(yscale).tickSize(-5,0);
+//        //.tickFormat("");
+////    var effWidth = width - margin;
+//
+//    svg.append("g")
+//       .attr("transform", "translate(" + gu.xLeft + ", "+ 0.0 + ")")
+//       .call(y_axis);
+//
+//    svg.append("g")
+//       .attr("transform", "translate(" + gu.xRight + ", "+ 0.0 + ")")
+//       .call(y_axisR);
+//
+//    var xmargin = 0.;
+//    svg.append("g")
+//            .attr("transform", "translate("+ xmargin +", " + gu.yBot  +")")
+//            .call(x_axis)
+//
+//    svg.append("g")
+//       .attr("transform", "translate("+xmargin+", " + gu.yTop + ")")
+//       .call(x_axisT);
+//
+//    svg.append("text")
+//       .attr("transform", "translate("+geo[0].xLeft/3+","+ geo[i].midY +")rotate(-90)")
+//       .attr("font-size", "12px")
+//       .attr("text-anchor", "middle")
+//       .text(cont + " [" + unit + "]");
+//
+//
+//    for(var j=0; j < nPoints; j++){
+//        svg.append("g").append("circle")
+//         .attr("cx", xscale(timex[j]) )
+//         .attr("cy", yscale(yData[j]) )
+//         .attr("r", 2)
+//         .attr("fill", laScale(yData[j]));
+////         .attr("fill", "red");
+//  }
+// }
+// fullBoxX += 10;
+// fullBoxY += 10;
+}
+
+function clearSelected(){
+    wishList = {};
+    console.info("current  wishList: "+ Object.keys(wishList) );
+}
+
+function markMe(name, eventual){
+    leName = name.name;
+     document.getElementById("wishes").style.visibility = "visible";
+    if( wishList[leName] == null){
+        console.info("marking LeName "+ leName );
+        wishList[leName] = 1;
+    } else {
+//        console.info("removing "+ leName + " from wishList");
+        delete wishList[leName];
+    }
+//    console.info("current  wishList "+ Object.keys(wishList) );
+}
 
 function makePlot1(name, eventual){
  var zoom = map.getZoom();
@@ -826,7 +1215,7 @@ function makePlot1(name, eventual){
 //    console.info("Time: " + i + ": " + data[tName][i] + ", " + timex[i]);
  }
 
- geo = plotGeometry(3, 500, 200);
+ geo = plotGeometry(3, 500, 200, 0);
  for (var i = 0; i < 3 ; i++){
     gu = geo[i];
     console.info("Plot " + i + " " +  gu.yTop +  " " +  gu.yBot);
@@ -881,7 +1270,7 @@ function makePlots(name, eventual){
 //    console.info("Time: " + i + ": " + data[tName][i] + ", " + timex[i]);
  }
 
- geo = plotGeometry(nPlots, cWidth, cHeight);
+ geo = plotGeometry(nPlots, cWidth, cHeight, 0);
  mint = d3.min(timex);
  maxt = d3.max(timex);
  xpadding = (maxt - mint)/20.;
@@ -1152,7 +1541,7 @@ function generateScatter(canvasID, sensorID, qID, qUnit, qUcd, yData, timex){
 
 // returns an object with nPlots objects which contain the location of each
 // plot.
-function plotGeometry(nPlots, swidth, sheight){
+function plotGeometry(nPlots, swidth, sheight, xRoom){
 //    console.info("plotGeo args: " + nPlots + " " + sheight  + " " + swidth);
     height = Number(sheight);
     width = Number(swidth);
@@ -1162,6 +1551,7 @@ function plotGeometry(nPlots, swidth, sheight){
     ymarginTop = height*0.08;
     if(ymarginTop < 40){ ymarginTop = 40; }
     ymarginBottom = ymarginTop;
+    ymarginTop += xRoom;
     spacing = ymarginTop/5;
     availableRoom = height - ymarginTop - ymarginBottom - (nPlots-1)*spacing;
     plotHeight = Math.floor(availableRoom/nPlots);
